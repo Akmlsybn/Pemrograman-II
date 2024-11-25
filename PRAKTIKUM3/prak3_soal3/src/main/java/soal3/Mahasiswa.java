44387944df28c4671aa4d2af98322272884317c9
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2024-11-25 22:31:52 +0800

    soal 3

--- PRAKTIKUM3/prak3_soal3/src/main/java/soal3/Mahasiswa.java
+++ PRAKTIKUM3/prak3_soal3/src/main/java/soal3/Mahasiswa.java
@@ -1,5 +1,20 @@
 package soal3;
 
-public class Mahasiswa {
+class Mahasiswa {
+    private String nama;
+    private String nim;
+
+    Mahasiswa (String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    String getNama() {
+        return nama;
+    }
+
+    String getNim() {
+        return nim;
+    }
 
 }
--- PRAKTIKUM3/prak3_soal3/src/main/java/soal3/Main.java
+++ PRAKTIKUM3/prak3_soal3/src/main/java/soal3/Main.java
@@ -1,7 +1,73 @@
 package soal3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner pilihan = new Scanner(System.in);
+
+        ArrayList<Mahasiswa> mahasiswa = new ArrayList<Mahasiswa>();
+
+        int pil;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Tambah Mahasiswa");
+            System.out.println("2. Hapus Mahasiswa berdasarkan NIM");
+            System.out.println("3. Cari Mahasiswa berdasarkan NIM");
+            System.out.println("4. Tampilkan Daftar Mahasiswa");
+            System.out.println("0. Keluar");
+
+            System.out.print("Pilihan: ");
+            pil = pilihan.nextInt();
+
+            switch (pil) {
+                case 0:
+                System.out.println("Terima Kasih!");
+                break;
+
+                case 1:
+                    System.out.print("Masukkan Nama Mahasiswa: ");
+                    String nama = pilihan.next();
+                    System.out.print("Masukkan NIM Mahasiswa (harus unik): ");
+                    String nim = pilihan.next();
+                    mahasiswa.add(new Mahasiswa(nama, nim));
+                    System.out.println("Mahasiswa " + nama + " ditambahkan.");
+                    break;
+            
+                case 2:
+                    System.out.print("Masukkan NIM Mahasiswa yang akan dihapus: ");
+                    String nimHapus = pilihan.next();
+                    for (int i = 0; i < mahasiswa.size(); i++) {
+                        if (mahasiswa.get(i).getNim().equals(nimHapus)) {
+                            mahasiswa.remove(i);
+                            System.out.println("Mahasiswa dengan NIM " + nimHapus + " dihapus.");
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Masukkan NIM: ");
+                    String nimCari = pilihan.next();
+                    for (int i = 0; i < mahasiswa.size(); i++){
+                        if(mahasiswa.get(i).getNim().equals(nimCari)){
+                            System.out.println("Mahasiswa dengan NIM yang dicari : " + nimCari);
+                            System.out.println("NIM : " + mahasiswa.get(i).getNim() + ", Nama: " + mahasiswa.get(i).getNama());
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Daftar Mahasiswa: ");
+                    for (int i = 0; i < mahasiswa.size(); i++){
+                        System.out.println("NIM : " + mahasiswa.get(i).getNim() + ", Nama: " + mahasiswa.get(i).getNama());
+                    }
+                    break;
+                default:
+                System.out.println("Coba lagi");
+            }
+        } while (pil != 0);
+        pilihan.close();
     }
 }
