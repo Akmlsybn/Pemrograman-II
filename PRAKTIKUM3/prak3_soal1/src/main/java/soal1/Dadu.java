bc4845c86bb0d6b1af0edbd7131aca58981b70bb
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2024-11-25 20:03:19 +0800

    class dadu

--- PRAKTIKUM3/prak3_soal1/src/main/java/soal1/Dadu.java
+++ PRAKTIKUM3/prak3_soal1/src/main/java/soal1/Dadu.java
@@ -3,31 +3,31 @@ package soal1;
 import java.util.LinkedList;
 import java.util.Random;
 
-public class Dadu {
+class Dadu {
 
     private LinkedList<Integer> angkaDadu;
 
-    public Dadu (){
+    Dadu (){
         angkaDadu = new LinkedList<>();
     }
 
-    public int acakNilai(){
+    int acakNilai(){
         Random random = new Random();
         int nilai = random.nextInt(6) + 1;
         return nilai;
     }
 
-    public LinkedList<Integer> getAngkaDadu(){
+    LinkedList<Integer> getAngkaDadu(){
         return angkaDadu;
     }
 
-    public void lemparanDadu(int n){
+    void lemparanDadu(int n){
         for (int i = 0; i < n; i++){
             angkaDadu.add(acakNilai());
         }
     }
 
-    public int hitungDadu(int n){
+    int hitungDadu(int n){
         int total = 0;
         for (int i = 0; i < n; i++){
             total += angkaDadu.get(i);
