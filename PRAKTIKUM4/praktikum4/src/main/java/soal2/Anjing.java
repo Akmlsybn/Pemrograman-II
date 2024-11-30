9bc77c1e1068ecb1bd566c137bc714159fb5b439
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2024-11-30 20:37:34 +0800

    -m

--- PRAKTIKUM4/praktikum4/src/main/java/soal2/Anjing.java
+++
@@ -1,9 +0,0 @@
-package soal2;
-import soal1.HewanPeliharaan;
-
-public class Anjing extends HewanPeliharaan {
-    
-    public Anjing(String n, String r) {
-        super(n, r);
-    } 
-}
--- PRAKTIKUM4/praktikum4/src/main/java/soal2/Kucing.java
+++
@@ -1,9 +0,0 @@
-package soal2;
-import soal1.HewanPeliharaan;
-
-public class Kucing extends HewanPeliharaan {
-    
-    public Kucing(String n, String r) {
-        super(n, r);
-    } 
-}
