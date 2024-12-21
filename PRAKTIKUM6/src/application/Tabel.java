10bedc1cda4d11db021b4b76487db915a842e98c
Akmallullail Sya'ban <2310817310010@mhs.ulm.ac.id>
2024-12-21 20:01:02 +0800

    update tabel

--- PRAKTIKUM6/src/application/Tabel.java
+++ PRAKTIKUM6/src/application/Tabel.java
@@ -16,7 +16,7 @@ public class Tabel extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        TableView<Mahasiswa> tabel = new TableView<>();
+        TableView<Mahasiswa> Tabel = new TableView<>();
 
         TableColumn<Mahasiswa, Integer> kolom1 = new TableColumn<>("ID");
         TableColumn<Mahasiswa, String> kolom2 = new TableColumn<>("NIM");
@@ -26,22 +26,22 @@ public class Tabel extends Application {
         kolom2.setCellValueFactory(new PropertyValueFactory<>("nim")); 
         kolom3.setCellValueFactory(new PropertyValueFactory<>("nama")); 
 
-        tabel.getColumns().add(kolom1);
-        tabel.getColumns().add(kolom2);
-        tabel.getColumns().add(kolom3);
+        Tabel.getColumns().add(kolom1);
+        Tabel.getColumns().add(kolom2);
+        Tabel.getColumns().add(kolom3);
 
-        tabel.getItems().add(new Mahasiswa(1, "2310817310010", "Akmallullail Sya'ban"));
-        tabel.getItems().add(new Mahasiswa(2, "2310817310011", "Muhammad Rizky"));
-        tabel.getItems().add(new Mahasiswa(3, "2310817120002", "Siti Ratna Dwinta Sari"));
-        tabel.getItems().add(new Mahasiswa(4, "2310817110015", "Akhmad Chaidar Ananda"));
-        tabel.getItems().add(new Mahasiswa(5, "2310817210020", "Muhammad Hasbiyan Rusyadi"));
-        tabel.getItems().add(new Mahasiswa(6, "2310817120001", "Dina Izzati Elfadheya"));
-        tabel.getItems().add(new Mahasiswa(7, "2310817210012", "Nazmi Hakim"));
-        tabel.getItems().add(new Mahasiswa(8, "2310817110006", "Adrian Bintang Saputera"));
-        tabel.getItems().add(new Mahasiswa(9, "2310817120014", "Aliya Raffa Naura Ayu"));
-        tabel.getItems().add(new Mahasiswa(10,"2310817210007", "Raymond Hariyono"));
+        Tabel.getItems().add(new Mahasiswa(1, "2310817310010", "Akmallullail Sya'ban"));
+        Tabel.getItems().add(new Mahasiswa(2, "2310817310011", "Muhammad Rizky"));
+        Tabel.getItems().add(new Mahasiswa(3, "2310817120002", "Siti Ratna Dwinta Sari"));
+        Tabel.getItems().add(new Mahasiswa(4, "2310817110015", "Akhmad Chaidar Ananda"));
+        Tabel.getItems().add(new Mahasiswa(5, "2310817210020", "Muhammad Hasbiyan Rusyadi"));
+        Tabel.getItems().add(new Mahasiswa(6, "2310817120001", "Dina Izzati Elfadheya"));
+        Tabel.getItems().add(new Mahasiswa(7, "2310817210012", "Nazmi Hakim"));
+        Tabel.getItems().add(new Mahasiswa(8, "2310817110006", "Adrian Bintang Saputera"));
+        Tabel.getItems().add(new Mahasiswa(9, "2310817120014", "Aliya Raffa Naura Ayu"));
+        Tabel.getItems().add(new Mahasiswa(10,"2310817210007", "Raymond Hariyono"));
 
-        VBox box = new VBox(tabel);
+        VBox box = new VBox(Tabel);
         Scene scene = new Scene(box);
 
         primaryStage.setScene(scene);
